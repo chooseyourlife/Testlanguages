7951d9debbe3b966aa584e3bc51f5e25f0a78463
Nigan <brodvey77@gmail.com>
2026-02-26 13:19:55 +0300

    add files conftest and test_items

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+# class TestGoodsHaveBasketButton:
+
+
+def test_goods_have_basket_button(link):
+    browser.get(link)
+    time.sleep(5)
+    browser.
